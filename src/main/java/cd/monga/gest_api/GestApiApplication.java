ca58c1c5aa40a01a265bc5ed4daeeb8636da2587
Mgreat01 <ephraimmonga5@gmail.com>
2026-04-06 02:09:17 +0200

    first com...

---
+++ src/main/java/cd/monga/gest_api/GestApiApplication.java
@@ -0,0 +1,13 @@
+package cd.monga.gest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestApiApplication.class, args);
+	}
+
+}
